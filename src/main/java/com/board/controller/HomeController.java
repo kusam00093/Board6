6c16c6f86fd260088ba8fb796778f9e59201e1c6
kusam00093 <yusanghun12@naver.com>
2024-03-29 14:29:25 +0900

    if문 추가

--- src/main/java/com/board/controller/HomeController.java
+++ src/main/java/com/board/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.board.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,15 +34,20 @@ public class HomeController {
 	// login(userid=sky&passwd=1234)
 	@RequestMapping("/Login")
 	public ModelAndView login(HttpServletRequest request) {
+	//	public ModelAndView login(@Param String userid, @Param String passwd, HttpServletRequest request) {
+		
 		
 		String userid = request.getParameter("userid");
 		String passwd = request.getParameter("passwd");
 		
 		UserVo userVo = userMapper.login(userid,passwd);
 		
+		if(userVo!=null) { //아이디와 암호가 일치하면
+			
 		HttpSession session = request.getSession();
 		session.setAttribute("login", userVo);
-		session.setMaxInactiveInterval(30*60);
+		session.setMaxInactiveInterval(30*60);  //30분 동안 유지
+		} 
 		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
